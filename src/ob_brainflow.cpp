255f781c7f745bbc17d0fe55c4ad1fba8352ab86
Chris Veigl <veigl@technikum-wien.at>
2022-10-30 13:41:17 +0100

    added timeout

--- src/ob_brainflow.cpp
+++ src/ob_brainflow.cpp
@@ -155,6 +155,7 @@ void bf_setparams(BRAINFLOWOBJ* st) {
     params.serial_port = std::string(st->serialport);
     params.ip_address = std::string(st->ipaddress);
     params.mac_address = std::string(st->macaddress);
+    params.timeout = st->timeout;
     params.ip_port = st->ipport;
 }
 
@@ -280,6 +281,7 @@ LRESULT CALLBACK BrainflowDlgHandler(HWND hDlg, UINT message, WPARAM wParam, LPA
         SetDlgItemText(hDlg, IDC_BF_MACADDRESS, st->macaddress);
         SetDlgItemText(hDlg, IDC_BF_IPADDRESS, st->ipaddress);
         SetDlgItemInt(hDlg, IDC_BF_IPPORT, st->ipport,FALSE);
+        SetDlgItemInt(hDlg, IDC_BF_TIMEOUT, st->timeout, FALSE);
 
         SetDlgItemText(hDlg, IDC_BF_CONFIG, st->bfConfigString);
 
@@ -323,7 +325,7 @@ LRESULT CALLBACK BrainflowDlgHandler(HWND hDlg, UINT message, WPARAM wParam, LPA
             GetDlgItemText(hDlg, IDC_BF_IPADDRESS, st->ipaddress, sizeof(st->ipaddress)-1);
             st->ipport = GetDlgItemInt(hDlg, IDC_BF_IPPORT, NULL, false);
             GetDlgItemText(hDlg, IDC_BF_MACADDRESS, st->macaddress, sizeof(st->macaddress)-1);
-
+            st->timeout = GetDlgItemInt(hDlg, IDC_BF_TIMEOUT, NULL, false);
             GetDlgItemText(hDlg, IDC_BF_CONFIG, st->bfConfigString, sizeof(st->bfConfigString) - 1);
 
             try
@@ -436,6 +438,7 @@ BRAINFLOWOBJ::BRAINFLOWOBJ(int num) : BASE_CL()
     strcpy(serialport, "COM4");
     strcpy(ipaddress, "192.168.4.1");
     ipport = 4567;
+    timeout = 0;
     strcpy(macaddress, "");
     strcpy(bfConfigString, "");
 
@@ -478,6 +481,7 @@ void BRAINFLOWOBJ::load(HANDLE hFile)
     load_property("macaddress", P_STRING, macaddress);
     load_property("show_position", P_INT, &show_position);
     load_property("show_extrachannels", P_INT, &show_extrachannels);
+    load_property("timeout", P_INT, &timeout);
     load_property("bfConfigString", P_STRING, bfConfigString);
 
 
@@ -506,6 +510,7 @@ void BRAINFLOWOBJ::save(HANDLE hFile)
     save_property(hFile, "macaddress", P_STRING, macaddress);
     save_property(hFile, "show_position", P_INT, &show_position);
     save_property(hFile, "show_extrachannels", P_INT, &show_extrachannels);
+    save_property(hFile, "timeout", P_INT, &timeout);
     save_property(hFile, "bfConfigString", P_STRING, bfConfigString);
 
     save_property(hFile, "archivefile", P_STRING, archivefile);
